24204852f632e4c51dc421f3a5312eb71973d4b1
Shawn Petros <arashcuzi@gmail.com>
2018-01-27 11:37:26 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+export default () => (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Welcome To MyHelloWorld</h1>
+    </header>
+    <p className="App-intro">
+      My name is Shawn Petros. Welcome, and hello!
+    </p>
+  </div>
+);
